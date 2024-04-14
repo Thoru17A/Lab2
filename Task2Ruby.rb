fe485a000134fb52c1efac93d2719f39822f28af
Thoru17A <163637659+Thoru17A@users.noreply.github.com>
2024-04-14 21:24:48 +0700

    Create Task2Ruby.rb

---
+++ Task2Ruby.rb
@@ -0,0 +1,22 @@
+def find_final(pairs)
+  start_count = {}
+  end_count = {}
+
+  # Заполняем словари начальных и конечных пунктов
+  pairs.each do |pair|
+    start_count[pair[0]] = (start_count[pair[0]] || 0) + 1
+    end_count[pair[1]] = (end_count[pair[1]] || 0) + 1
+  end
+
+  # Ищем пункт, который не является начальным для какой-либо пары
+  end_count.keys.each do |end|
+    if !start_count.key?(end)
+      return end
+    end
+  end
+
+  # Если все пункты являются начальными для какой-либо пары, возвращаем сообщение о необходимости обращения к специалисту
+  return "обратитесь к специалисту"
+end
+
+puts find_final([["Новосибирск", "Новосибирск"]])

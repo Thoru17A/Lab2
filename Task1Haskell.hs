8c69905817b621c8815f4065d1fd6311f16acb2f
Thoru17A <163637659+Thoru17A@users.noreply.github.com>
2024-04-18 21:51:26 +0700

    Update Task1Haskell.hs

--- Task1Haskell.hs
+++ Task1Haskell.hs
@@ -1,24 +1,19 @@
-import Data.List (elemIndex)
+module Main where
 
 main :: IO ()
 main = do
-    let N = 10
-        X = 1
-        S = "1000100111"
+    let n = 10
+        x = 1
+        s = "1000100111"
+    putStrLn $ if allCooperatives n x s then "Yes" else "No"
 
-    if AllCooperatives N X S
-        then putStrLn "Yes"
-        else putStrLn "No"
+allCooperatives :: Int -> Int -> String -> Bool
+allCooperatives n x s = all (canBeRepaired n x s) [0..(n-1)]
 
--- Function to check if all towers can be repaired
-AllCooperatives :: Int -> Int -> String -> Bool
-AllCooperatives N X S = all (`canBeRepaired` N X S) [0..(N-1)]
-
--- Function to check if a tower can be repaired
 canBeRepaired :: Int -> Int -> String -> Int -> Bool
-canBeRepaired N X S i
-    | S !! i == '1' = True
+canBeRepaired n x s i
+    | s !! i == '1' = True
     | otherwise = leftTowerWorks || rightTowerWorks
     where
-        leftTowerWorks = if i - X < 0 then False else S !! (i - X) == '1'
-        rightTowerWorks = if i + X >= N then False else S !! (i + X) == '1'
+        leftTowerWorks = any (== '1') $ take (i - x) $ drop (max 0 (i - x)) s
+        rightTowerWorks = any (== '1') $ take (x + 1) $ drop (i + 1) s

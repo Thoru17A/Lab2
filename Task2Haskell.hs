76a23692f389976cc9d108128c345f87e66aeb91
Thoru17A <163637659+Thoru17A@users.noreply.github.com>
2024-04-18 21:52:30 +0700

    Update Task2Haskell.hs

--- Task2Haskell.hs
+++ Task2Haskell.hs
@@ -1,13 +1,15 @@
-import Data.Map (Map, insert, member, keys, fromList)
-
-findFinal :: [[String]] -> String
-findFinal pairs = case lookup endCount startCount of
-    Nothing -> end
-    _ -> "обратитесь к специалисту"
-  where
-    startCount = fromList $ map (\(a, _) -> (a, 1)) pairs
-    endCount = fromList $ map (\(_, b) -> (b, 1)) pairs
-    end = head $ keys endCount
+import qualified Data.Map as Map
 
 main :: IO ()
-main = print $ findFinal [["Новосибирск", "Новосибирск"]]
+main = do
+    let pairs = [["Новосибирск", "Новосибирск"]]
+    putStrLn $ findFinal pairs
+
+findFinal :: [[String]] -> String
+findFinal pairs =
+    let startCount = foldl (\m [start, _] -> Map.insertWith (+) start 1 m) Map.empty pairs
+        endCount = foldl (\m [_, end] -> Map.insertWith (+) end 1 m) Map.empty pairs
+        nonStartingEnds = filter (`Map.notMember` startCount) $ Map.keys endCount
+    in case nonStartingEnds of
+        [] -> "обратитесь к специалисту"
+        (end:_) -> end
